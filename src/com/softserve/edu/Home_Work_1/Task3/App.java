2afec0468c4bfba425993a2255ccebfaa6c0798a
Alexsander Tsova <tsova@bk.ru>
2017-01-04 00:25:54 +0200

    do home task #3

---
+++ src/com/softserve/edu/Home_Work_1/Task3/App.java
@@ -0,0 +1,101 @@
+package com.softserve.edu.Home_Work_1.Task3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class App {
+    static int i = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        HashMap<Double, String> sTriagle = new HashMap<Double, String>();
+
+
+        while (true){
+            //Read information about Triangle
+            System.out.println("Please enter data of triangle");
+
+            String triangle = reader.readLine();
+
+            String[] arrayStrings = triangle.split(",");
+            double a;
+            double b;
+            double c;
+
+            try {
+                a = Integer.parseInt(arrayStrings[1].trim());
+                b = Integer.parseInt(arrayStrings[2].trim());
+                c = Integer.parseInt(arrayStrings[3].trim());
+            }catch (Exception e){
+                System.out.println("Help");
+                return;
+            }
+
+            //Calculate square of Triangle
+            double p = (double) 1/2 * (a + b + b);
+            double s = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+
+            // add Triangles to HashMap
+            sTriagle.put(s, arrayStrings[0].trim());
+
+
+            //Function exit
+            System.out.println("Do you want to enter new triangle");
+            String exit = reader.readLine();
+
+            if (exit.toLowerCase().equals("y") || exit.toLowerCase().equals("yes")){
+                System.out.println();
+            }else {
+                //Publish information about Triangles
+                Map<Double, String> sortedMap = new TreeMap<Double, String>(Collections.reverseOrder());
+                sortedMap.putAll(sTriagle);
+
+                System.out.println("============= Triangles list: ===============");
+                for (Map.Entry<Double, String> temp : sortedMap.entrySet()){
+                    i = i + 1;
+                    System.out.println(i + "." + "[" + temp.getValue() + "]:" + " " + temp.getKey() + " cm");
+                }
+
+                return;
+            }
+        }
+
+//        System.out.println("Введите первый треугольник");
+//        String triangle = reader.readLine();
+//
+//        String[] arrayStrings = triangle.split(",");
+//        double a = Integer.parseInt(arrayStrings[1].trim());
+//        double b = Integer.parseInt(arrayStrings[2].trim());
+//        double c = Integer.parseInt(arrayStrings[3].trim());
+//
+//        double p = (double) 1/2 * (a + b + b);
+//        double s = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+//
+//
+//        //второй треугольник
+//        System.out.println("Введите второй треугольник");
+//        String triangle2 = reader.readLine();
+//
+//        String[] arrayStrings2 = triangle2.split(",");
+//
+//        double p2 = 1/2*(Integer.parseInt(arrayStrings2[1].trim()) + Integer.parseInt(arrayStrings2[2].trim()) + Integer.parseInt(arrayStrings2[3].trim()));
+//        double s2 = Math.sqrt(p * (p - Integer.parseInt(arrayStrings2[1].trim())) * (p - Integer.parseInt(arrayStrings2[2].trim())) * (p - Integer.parseInt(arrayStrings2[3].trim())));
+//
+//        //HashMap<Double, String> sTriagle = new HashMap<Double, String>();
+//        sTriagle.put(s, arrayStrings[0].trim());
+//        sTriagle.put(s2, arrayStrings2[0].trim());
+//
+//        Map<Double, String> sortedMap = new TreeMap<Double, String>();
+//        sortedMap.putAll(sTriagle);
+//
+//        for (Map.Entry<Double, String> temp : sTriagle.entrySet()){
+//            System.out.println(temp.getKey() + " " + temp.getValue());
+//        }
+
+    }
+}
